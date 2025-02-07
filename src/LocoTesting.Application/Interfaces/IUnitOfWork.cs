36e8c22d7c4960f7a7e3009837ddd85465082e36
Belal Abdibaliev <belalabdibaliev55@gmail.com>
2025-02-07 23:08:19 +0600

    Added UnitOfWork class

---
+++ src/LocoTesting.Application/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using LocoTesting.Application.Interfaces.Repositories;
+
+namespace LocoTesting.Application.Interfaces;
+
+public interface IUnitOfWork
+{
+    ITestRepository Tests { get; }
+    IOptionRepository Options { get; }
+    IQuestionRepository Questions { get; }
+    
+    Task<int> SaveChangesAsync();
+}
---
+++ src/LocoTesting.Infrastructure/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using LocoTesting.Application.Interfaces;
+using LocoTesting.Application.Interfaces.Repositories;
+using LocoTesting.Infrastructure.DataAccess;
+using LocoTesting.Infrastructure.Repositories;
+
+namespace LocoTesting.Infrastructure;
+
+public class UnitOfWork: IUnitOfWork
+{
+    private readonly ApplicationDbContext _context;
+    public ITestRepository Tests { get; }
+    public IOptionRepository Options { get; }
+    public IQuestionRepository Questions { get; }
+
+    public UnitOfWork(ApplicationDbContext context, ITestRepository tests, IOptionRepository options, IQuestionRepository questions)
+    {
+        _context = context;
+        Tests = tests;
+        Options = options;
+        Questions = questions;
+    }
+    
+    public async Task<int> SaveChangesAsync()
+    {
+        return await _context.SaveChangesAsync();
+    }
+}
