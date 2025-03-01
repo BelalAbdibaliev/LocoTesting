a60a4e031d599427d8e1c1fc4bea469842809031
Belal Abdibaliev <belalabdibaliev55@gmail.com>
2025-03-01 10:56:00 +0600

    Set string.Empty as a default value for string type entities

--- src/LocoTesting.Domain/Models/Question.cs
+++ src/LocoTesting.Domain/Models/Question.cs
@@ -3,8 +3,8 @@
 public class Question
 {
     public int Id { get; set; }
-    public string Text { get; set; }
-    public string? Content { get; set; }
+    public string Text { get; set; } = string.Empty;
+    public string? Content { get; set; } = string.Empty;
     
     public List<AnswerOption>? AnswerOptions { get; set; }
     public int TestId { get; set; }
--- src/LocoTesting.Domain/Models/Test.cs
+++ src/LocoTesting.Domain/Models/Test.cs
@@ -3,8 +3,8 @@
 public class Test
 {
     public int Id { get; set; }
-    public string Title { get; set; }
-    public string Description { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
     
     public List<Question> Questions { get; set; } = new List<Question>();
 }
