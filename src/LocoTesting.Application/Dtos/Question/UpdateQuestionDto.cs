16e66237fbf68b98740b89b0670c88ab3baadf99
Belal Abdibaliev <belalabdibaliev55@gmail.com>
2025-03-04 22:34:55 +0600

    Created UpdateQuestionDto.cs

---
+++ src/LocoTesting.Application/Dtos/Question/UpdateQuestionDto.cs
@@ -0,0 +1,8 @@
+﻿namespace LocoTesting.Application.Dtos.Question;
+
+public class UpdateQuestionDto
+{
+    public int Id { get; set; }
+    public string? Text { get; set; }
+    public string? Content { get; set; }
+}
