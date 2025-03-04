c39264df15ef266cab6ea5cfa4d26a280e167841
Belal Abdibaliev <belalabdibaliev55@gmail.com>
2025-03-04 22:34:29 +0600

    Made Text property nullable in Question entity

--- src/LocoTesting.Domain/Entities/Question.cs
+++ src/LocoTesting.Domain/Entities/Question.cs
@@ -3,7 +3,7 @@
 public class Question
 {
     public int Id { get; set; }
-    public string Text { get; set; } = string.Empty;
+    public string? Text { get; set; } = string.Empty;
     public string? Content { get; set; } = string.Empty;
     
     public List<AnswerOption>? AnswerOptions { get; set; }
